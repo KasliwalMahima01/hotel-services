6c3f1f582f19db9396d59e6d58b4254439c20cc5
KasliwalMahima01 <101016953+KasliwalMahima01@users.noreply.github.com>
2022-03-09 14:36:24 +0530

    Add files via upload

---
+++ backend.js
@@ -0,0 +1,14 @@
+function Reserves() {
+    alert("Your Room is reserved!!!");
+}
+
+function SendMessage() {
+    confirm("Your responce is recorded");
+}
+function CallUs() {
+    alert("Contact us on 9527674438");
+}
+
+function EmailUs() {
+    alert("Email us on stayhotel@gmail.com");
+}
